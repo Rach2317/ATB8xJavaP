b5dc2dc6ffd3cfe9001580c8318bbacb67e0702d
Rachana <rachanaganachari2307@gmail.com>
2024-09-23 02:07:14 +0530

    commit 6th sep

---
+++ src/sept/ex09092024/Lab006.java
@@ -0,0 +1,10 @@
+package sept.ex09092024;
+
+public class Lab006
+{
+    public static void main(String[] args)
+    {
+        boolean is_Mohit_Married = true;
+        System.out.println(is_Mohit_Married);
+    }
+}
