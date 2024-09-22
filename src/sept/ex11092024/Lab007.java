35359f76e9bfd2cd7b7c9d94439a10492ddc6770
Rachana <rachanaganachari2307@gmail.com>
2024-09-23 02:58:15 +0530

    commit 11th sep

---
+++ src/sept/ex11092024/Lab007.java
@@ -0,0 +1,15 @@
+package sept.ex11092024;
+
+public class Lab007
+{
+    public static void main(String[] args)
+    {
+       boolean $ = true;
+       boolean _ = true;
+       boolean Rachana_is_good_human = true;
+       boolean Rachana = true;
+       boolean _123 = true;
+        System.out.println(_123);
+
+    }
+}
