f0f924fe8c20ccb5cf7533f67c82b673a70f7fa2
Rachana <rachanaganachari2307@gmail.com>
2024-09-23 01:24:22 +0530

    commit 6th sep

---
+++ src/sept/ex06092024/Lab004.java
@@ -0,0 +1,14 @@
+package sept.ex06092024;
+
+public class Lab004
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Kindness never goes out of fashion");
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+
+    }
+}
